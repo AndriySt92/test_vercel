42a82c4d3b2ad035f1cdf2e5488be6fa4b46047f
Andriy <aaandriy92@gmail.com>
2025-12-02 23:36:59 +0200

    fix use set new version

--- src/index.ts
+++ src/index.ts
@@ -3,64 +3,18 @@ import http from "http";
 dotenv.config();
 
 import cookieParser from "cookie-parser";
-import cors, { CorsOptions } from "cors";
+import cors from "cors";
 import express from "express";
 
 import connectDB from "./config/connectDb";
+import getCorsOptions from "./config/cors";
 import { errorHandler } from "./middlewares";
 import { AdminRoutes, BookingRoutes, PhotoRoutes } from "./routes";
 import { CustomError } from "./utils";
 
 const app = express();
 
-// const getCorsOptions = (): CorsOptions => {
-//   let allowedOrigins: string[];
-
-//   try {
-//     allowedOrigins = process.env.ALLOWED_ORIGINS ? JSON.parse(process.env.ALLOWED_ORIGINS) : [];
-//   } catch {
-//     allowedOrigins = [];
-//   }
-
-//   return {
-//     origin: function (origin, callback) {
-//       if (!origin) return callback(null, true);
-
-//       if (allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         console.warn(`CORS blocked for origin: ${origin}`);
-//         callback(null, false);
-//       }
-//     },
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "X-App-Version"],
-
-//   };
-// };
-
-const allowedOrigins = process.env.ALLOWED_ORIGINS ? JSON.parse(process.env.ALLOWED_ORIGINS) : [];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        console.warn(`CORS blocked for origin: ${origin}`);
-        callback(null, false);
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "X-App-Version"],
-  }),
-);
-// app.use(cors(getCorsOptions()));
+app.use(cors(getCorsOptions()));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
