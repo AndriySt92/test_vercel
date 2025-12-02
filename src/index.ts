8a7b4944b8c78524e60c65c804768e98876186aa
Andriy <aaandriy92@gmail.com>
2025-12-02 22:38:36 +0200

    fix cors back up

--- src/index.ts
+++ src/index.ts
@@ -13,43 +13,17 @@ import { CustomError } from "./utils";
 
 const app = express();
 
-const getCorsOptions = (): CorsOptions => {
-  let allowedOrigins: string[];
+// const getCorsOptions = (): CorsOptions => {
+//   let allowedOrigins: string[];
 
-  try {
-    allowedOrigins = process.env.ALLOWED_ORIGINS ? JSON.parse(process.env.ALLOWED_ORIGINS) : [];
-  } catch {
-    allowedOrigins = [];
-  }
+//   try {
+//     allowedOrigins = process.env.ALLOWED_ORIGINS ? JSON.parse(process.env.ALLOWED_ORIGINS) : [];
+//   } catch {
+//     allowedOrigins = [];
+//   }
 
-  return {
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        console.warn(`CORS blocked for origin: ${origin}`);
-        callback(null, false);
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "X-App-Version"],
-
-  };
-};
-
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://andriyst92.github.io",
-//   "https://test-vercel-six-beryl-85.vercel.app",
-// ];
-
-// app.use(
-//   cors({
+//   return {
 //     origin: function (origin, callback) {
-//       // Allow requests with no origin (like mobile apps or curl)
 //       if (!origin) return callback(null, true);
 
 //       if (allowedOrigins.includes(origin)) {
@@ -62,9 +36,35 @@ const getCorsOptions = (): CorsOptions => {
 //     credentials: true,
 //     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 //     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "X-App-Version"],
-//   }),
-// );
-app.use(cors(getCorsOptions()));
+
+//   };
+// };
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://andriyst92.github.io",
+  "https://test-vercel-six-beryl-85.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.warn(`CORS blocked for origin: ${origin}`);
+        callback(null, false);
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "X-App-Version"],
+  }),
+);
+// app.use(cors(getCorsOptions()));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
